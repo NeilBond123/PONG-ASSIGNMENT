db3fc1414d32362607f621ef8f8f08b9cca230e7
Bondleee <32422568+Bondleee@users.noreply.github.com>
2017-12-06 00:11:46 +0100

    General level layout added
    the basic layout for each level added to level 1. other levels will use this same layout only with some more additional game objects.

---
+++ PONG GAME/New Unity Project/Assets/Scripts/Paddle1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle1 : MonoBehaviour {
+    public float speed = 30;
+
+    void FixedUpdate()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ PONG GAME/New Unity Project/Assets/Scripts/Paddle2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle2 : MonoBehaviour {
+    public float speed = 30;
+
+    void FixedUpdate()
+    {
+        float v = Input.GetAxis("Mouse Y");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+     
+	}
+}
