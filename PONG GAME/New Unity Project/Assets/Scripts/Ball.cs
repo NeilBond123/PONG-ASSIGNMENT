3f9503509151b6d5cb6256b03be69912661995e1
Bondleee <32422568+Bondleee@users.noreply.github.com>
2017-12-08 16:19:08 +0100

    Ball script added
    Implemented ball's script using features such as velocity, direction etc.

--- PONG GAME/New Unity Project/Assets/Scripts/Ball.cs
+++ PONG GAME/New Unity Project/Assets/Scripts/Ball.cs
@@ -4,10 +4,53 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float speed = 30;
+
+    float onHit(Vector2 ballPos, Vector2 racketPos,
+                float racketHeight)
+    {
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        // Hit the left Racket?
+        if (col.gameObject.name == "paddle1")
+        {
+            // Get position of hit
+            float y = onHit(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction
+            Vector2 dir = new Vector2(1, y).normalized;
+
+            // Velocity
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        // Right paddle
+        if (col.gameObject.name == "paddle2")
+        {
+            // Get position of hit
+            float y = onHit(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            // Velocity
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        // Velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
 	
 	// Update is called once per frame
 	void Update () {
