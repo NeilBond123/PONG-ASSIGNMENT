77eee4bd53457789e34b5cf365727e70faeea716
Bondleee <32422568+Bondleee@users.noreply.github.com>
2017-12-09 13:32:00 +0100

    Player divider added
    added line in the middle of the pong layout to divide both players' areas from each other. Work on menu button functionalities started.

---
+++ PONG GAME/New Unity Project/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    public void startLevel()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
