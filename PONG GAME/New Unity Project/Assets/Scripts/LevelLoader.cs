b61536b432d0f428c8ca68da6c1becf340615943
Bondleee <32422568+Bondleee@users.noreply.github.com>
2017-12-11 11:15:17 +0100

    Menu finalized. Levels and final scene added
    Added code to the Menu screen buttons to make it fully functional. LevelLoader script further improved to switch to next scene/level according to current level (still work in progress).

--- PONG GAME/New Unity Project/Assets/Scripts/LevelLoader.cs
+++ PONG GAME/New Unity Project/Assets/Scripts/LevelLoader.cs
@@ -5,11 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour {
 
-    public void startLevel()
+    public void startLevel() // This function starts on start button click and once player scores are reached
     {
-        SceneManager.LoadScene("Level1");
+        // The following line gets the current scene's ID
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentScene == 0) // 0 refers to the Menu level ID.
+        {
+            SceneManager.LoadScene(1); // Load first level via level ID
+        }
+
+        // The following if statements are called when the scores are reached. 
+        // They check if the current level is either level 1, 2, or 3.
+
+        else if (currentScene == 1) // If score reached and current scene is level1
+        {
+            SceneManager.LoadScene(2); // Load level 2
+        }
+
+        else if (currentScene == 2) // If score reached and current scene is level2
+        {
+            SceneManager.LoadScene(3); // Load level 3
+        }
+
+        else if (currentScene == 3) // If final score reached and current scene is level 3
+        {
+            SceneManager.LoadScene(4); // Load winner screen
+        }
     }
 	// Use this for initialization
+
 	void Start () {
 		
 	}
